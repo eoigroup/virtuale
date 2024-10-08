605dcbd13c828db46b9080b3b819f2e9691ee170
best3217 <kjav.spt@gmail.com>
2024-10-08 19:47:35 +0200

    feat: add preferences title

--- components/preferences/preferences.tsx
+++ components/preferences/preferences.tsx
@@ -1,9 +1,14 @@
 import React from "react";
 import ThemeSettings from "./theme-settings";
+import { Typography } from "../ui/typography";
 
 const Preferences = () => {
   return (
     <div className="w-full flex flex-col gap-4">
+      <Typography variant={"h3"} className="mb-3">
+        Preferences
+      </Typography>
+
       <ThemeSettings />
     </div>
   );
