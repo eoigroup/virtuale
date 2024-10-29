dd7874782a8404054aab400f95a3f7a6ac873404
eoigroup <168847056+eoigroup@users.noreply.github.com>
2024-10-29 17:42:46 +0000

    New: Static Cards / react-icons

--- components/persona-list/persona-list.tsx
+++ components/persona-list/persona-list.tsx
@@ -2,6 +2,7 @@
 
 import React from "react";
 import PersonaCard from "../persona-card/persona-card";
+import StaticCard from "../static-card/static-card";
 import PersonaLoading from "./persona-loading";
 import { Swiper, SwiperSlide } from "swiper/react";
 import { FreeMode, Navigation } from "swiper/modules";
@@ -119,7 +120,60 @@ const PersonaList = ({
             </SwiperSlide>
           ))}
         </Swiper>
-      </div>
+      </div> 
+
+            <div className="mt-12 mb-10">
+              <Typography variant={"h5"} className="mb-4 ml-4 pt-10 border-t border-gray-800" >   
+                More Info
+              </Typography>
+              <div className="flex flex-wrap">
+                {/* Left Column */}
+                <div className="w-full md:w-1/2 flex flex-col"> 
+                <div className="flex w-full items-stretch"> {/* Ensure equal height */}
+                    <div className="flex-1 p-2"> {/* Suggest */}
+                      <StaticCard title="Suggest Persona" desc="Share your ideas to add/enhance our personas! Your feedback helps us improve and innovate." href="/faq" />
+                    </div>
+                    <div className="flex-1 p-2"> {/* FAQ */}
+                      <StaticCard title="FAQ" desc="Have questions? We have the answers!" href="/faq" />
+                    </div>
+                  </div>
+                  <div className="flex w-full mt-2">
+                    <div className="w-1/2 p-2"> {/* New Card 1 */}
+                      <StaticCard title="Partnerships" desc="Explore collaboration opportunities." href="/partnerships" />
+                    </div>
+                    <div className="w-1/2 p-2"> {/* New Card 2 */}
+                      <StaticCard title="Latest News" desc="Stay updated with our latest announcements." href="/news" />
+                    </div>
+                  </div>
+                  <div className="hidden md:flex w-full mt-2 "> {/* Additional Row for About and Legal */}
+                    <div className="w-1/2 p-2"> {/* About */}
+                      <StaticCard title="Company" desc="" href="" />
+                    </div>
+                    <div className="w-1/2 p-2"> {/* Legal */}
+                      <StaticCard title="Jargon" desc="" href="" />
+                    </div>
+                  </div>
+                </div>
+
+                {/* Community Card */}
+                <div className="w-full md:w-1/2 p-2 flex"> 
+                  <StaticCard title="Community" desc="Connect with fellow users, share your experiences, and stay updated with us!" className="flex-1 h-full" />
+                </div>
+
+                <div className="w-full md:w-1/2 flex flex-col"> 
+                  <div className="flex w-full mt-2 md:hidden"> {/* Additional Row for About and Legal */}
+                    <div className="w-1/2 p-2"> {/* About */}
+                      <StaticCard title="Company" desc="" href="" />
+                    </div>
+                    <div className="w-1/2 p-2"> {/* Legal */}
+                      <StaticCard title="Jargon" desc="" href="" />
+                    </div>
+                  </div>
+                </div>
+
+              </div>
+            </div>
+
     </>
   );
 };
---
+++ components/static-card/static-card.tsx
@@ -0,0 +1,129 @@
+import React from "react";
+import { Typography } from "../ui/typography";
+import Link from "next/link";
+import { FaInstagram, FaReddit, FaTwitter, FaTiktok } from 'react-icons/fa';
+import { SiX } from 'react-icons/si'; // Use SiX for the X icon
+
+
+
+interface StaticCardProps {
+  title: string;
+  desc: string;
+  href?: string; // Make href optional
+  className?: string; // Add className prop
+}
+
+const StaticCard: React.FC<StaticCardProps> = ({ title, desc, href, className }) => {
+  const renderContent = () => {
+    if (title === "Community") {
+      return (
+        <div className="flex flex-col h-full justify-between p-10">
+          <div className="flex flex-col items-center mb-4">
+            <Typography variant="large" className=" text-center text-lg font-semibold mb-2">
+            Unite with a Like-Minded Community
+            </Typography>
+            {desc && (
+              <Typography variant="body" className="text-gray-400 mb-2 text-center">
+                {desc}
+              </Typography>
+            )}
+          </div>
+          <div className="flex flex-wrap justify-center gap-4 items-center flex-1">
+          <div className="flex justify-center gap-6">
+                <a href="https://instagram.com" target="_blank" rel="noopener noreferrer">
+                  <FaInstagram className="h-10 w-10" />
+                </a>
+                <a href="https://reddit.com" target="_blank" rel="noopener noreferrer">
+                  <FaReddit className="h-10 w-10" />
+                </a>
+                <a href="https://x.com" target="_blank" rel="noopener noreferrer">
+                  <SiX className="h-10 w-10" />
+                </a>
+                <a href="https://tiktok.com" target="_blank" rel="noopener noreferrer">
+                  <FaTiktok className="h-10 w-10" />
+                </a>
+            </div>
+          </div>
+        </div>
+      );
+    } else if (title === "Jargon") {
+      return (
+        <div className="flex flex-col h-full">
+          <Typography variant="small" className="text-lg font-semibold mb-1">
+            {title}
+          </Typography>
+          {desc && (
+            <Typography variant="xsmall" className="text-gray-400 mb-2">
+              {desc}
+            </Typography>
+          )}
+          <ul className="pl-0">
+            <li className="text-gray-500 text-sm mb-1">
+              <Link href="/privacy-policy" className="text-blue-500 hover:underline">Privacy Policy</Link>
+            </li>
+            <li className="text-gray-500 text-sm mb-1">
+              <Link href="/terms-of-service" className="text-blue-500 hover:underline">Terms of Service</Link>
+            </li>
+            <li className="text-gray-500 text-sm mb-1">
+              <Link href="/cookies" className="text-blue-500 hover:underline">Cookies</Link>
+            </li>
+          </ul>
+        </div>
+      );
+    } else if (title === "Company") {
+      return (
+        <div className="flex flex-col h-full">
+          <Typography variant="small" className="text-lg font-semibold mb-1">
+            {title}
+          </Typography>
+          {desc && (
+            <Typography variant="xsmall" className="text-gray-400 mb-2">
+              {desc}
+            </Typography>
+          )}
+          <ul className="pl-0">
+            <li className="text-gray-500 text-sm mb-1">
+              <Link href="/about" className="text-blue-500 hover:underline">About</Link>
+            </li>
+            <li className="text-gray-500 text-sm mb-1">
+              <Link href="/careers" className="text-blue-500 hover:underline">Careers</Link>
+            </li>
+            <li className="text-gray-500 text-sm mb-1">
+              <Link href="/contact" className="text-blue-500 hover:underline">Contact Us</Link>
+            </li>
+          </ul>
+        </div>
+      );
+    }
+
+    // Default card content
+    return (
+      <div className="flex flex-col h-full">
+        <Typography variant="small" className="text-lg font-semibold mb-1 leading-none">
+          {title}
+        </Typography>
+        {desc && (
+          <Typography variant="xsmall" className="text-gray-400 line-clamp-3 text-ellipsis overflow-hidden whitespace-normal break-anywhere mb-2">
+            {desc}
+          </Typography>
+        )}
+      </div>
+    );
+  };
+
+  const cardContent = (
+    <div className={`block rounded-3xl bg-surface-elevation-2 backdrop-blur-lg p-4 relative h-full ${className}`}>
+      {renderContent()}
+    </div>
+  );
+
+  return href ? (
+    <Link href={href}>
+      {cardContent}
+    </Link>
+  ) : (
+    cardContent
+  );
+};
+
+export default StaticCard;
