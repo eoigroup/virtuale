84e3ed1b26bad4ff78129e00986b31f6ce07dd8b
eoigroup <168847056+eoigroup@users.noreply.github.com>
2024-10-29 23:58:30 +0000

    new: pages

---
+++ app/privacy/page.tsx
@@ -0,0 +1,19 @@
+"use client";
+
+import React from 'react';
+import { Typography } from '../../components/ui/typography'; // Adjust the path as necessary
+
+const CompanyPage = () => {
+  return (
+    <div className="container mx-auto p-4">
+      <Typography variant="h1" className="text-2xl font-bold mb-6">
+        Partnerships 
+      </Typography>
+      <Typography variant="body" className="text-gray-700">
+      desc text
+      </Typography>
+    </div>
+  );
+};
+
+export default CompanyPage;
