bfc805d9546770c3c3830b617c5226ef2d3c40cd
eoigroup <168847056+eoigroup@users.noreply.github.com>
2024-10-29 15:49:50 +0000

    update: banner persona selection

--- components/banner/banner_orignew.tsx
+++ components/banner/banner_orignew.tsx
@@ -8,42 +8,20 @@ import { Skeleton } from "../ui/skeleton";
 import { motion } from "framer-motion";
 import { IPersona } from "@/types/persona";
 
-const videoLinks = [
-  "https://media.identica.ai/media/virtuale/train.mp4",
-  "https://media.identica.ai/media/virtuale/scfiland.mp4",
-  "https://media.identica.ai/media/virtuale/girlrock.mp4",
-  "https://media.identica.ai/media/virtuale/alita_rise.mp4",
+const list = [
+  {
+    title: "Learn Something New Today",
+    videoLink: "https://media.identica.ai/media/virtuale/train.mp4",
+  },
+  {
+    title: "Where Conversations Come to Life",
+    videoLink: "https://media.identica.ai/media/virtuale/scfiland.mp4",
+  },
+  {
+    title: "Your World of Personal Conversations",
+    videoLink: "https://media.identica.ai/media/virtuale/girlrock.mp4",
+  },
 ];
-
-const titles = [
-  "Learn Something New Today",
-  "Where Conversations Come to Life",
-  "Your World of Personal Conversations",
-  "Where Every Chat Has Character",
-  "Engage with the Future",
-  "Discover New Horizons",
-  "Converse with AI",
-  "The Next Level of Interaction",
-  "Unlock the Power of Dialogue",
-  "Explore Endless Possibilities",
-  "Your Voice, Your Choice",
-  "Beyond Chat, Beyond AI - It's Personal",
-  "Find Your Perfect Conversation",
-  "Personas for Every Purpose",
-  "Chat with Purpose, Connect with Personality",
-  "Your Perfect Chat Partner Awaits",
-  "Discover Your Next Great Conversation",
-  "Your Trusted Circle of Support",
-  "Available Where Understanding Meets You",
-  "JL",
-];
-
-
-// Function to get random titles
-const getRandomTitles = (count: number): string[] => {
-  const shuffledTitles = [...titles].sort(() => 0.5 - Math.random());
-  return shuffledTitles.slice(0, count);
-};
 
 const Banner = ({
   personas,
@@ -53,53 +31,40 @@ const Banner = ({
   loading: boolean;
 }) => {
   const [activeIndex, setActiveIndex] = useState(0);
-  const [videoCurrentTimes, setVideoCurrentTimes] = useState<number[]>(Array(videoLinks.length).fill(0));
-  const videoRefs = useRef<(HTMLVideoElement | null)[]>(Array(videoLinks.length).fill(null));
-  const [selectedPersonas, setSelectedPersonas] = useState<IPersona[]>([]);
-  const [randomTitles, setRandomTitles] = useState(getRandomTitles(3)); // Select random titles
+  const [videoCurrentTimes, setVideoCurrentTimes] = useState<number[]>(Array(list.length).fill(0));
+  const videoRefs = useRef<(HTMLVideoElement | null)[]>(Array(list.length).fill(null));
+  const activeItem = list[activeIndex];
+  const itemsPerPage = 2;
+  const startIndex = activeIndex * itemsPerPage;
+  const endIndex = startIndex + itemsPerPage;
+  const _personas = personas
+    .filter((p) => p.virtuale_hero)
+    .slice(startIndex, endIndex);
 
   useEffect(() => {
     if (loading) return;
 
-    const selectRandomPersonas = () => {
-      const filteredPersonas = personas.filter((p) => p.virtuale_hero);
-      if (filteredPersonas.length < 2) {
-        setSelectedPersonas(filteredPersonas);
-      } else {
-        const randomIndices = new Set<number>();
-        while (randomIndices.size < 2) {
-          randomIndices.add(Math.floor(Math.random() * filteredPersonas.length));
-        }
-        const selectedIndices = Array.from(randomIndices);
-        setSelectedPersonas(selectedIndices.map(index => filteredPersonas[index]));
-      }
-    };
-
-    selectRandomPersonas();
-
     const interval = setInterval(() => {
-      const nextIndex = (activeIndex + 1) % videoLinks.length;
+      const nextIndex = (activeIndex + 1) % list.length;
       setActiveIndex(nextIndex);
-      setRandomTitles(getRandomTitles(3)); // Update random titles on index change
-      selectRandomPersonas(); // Select new random personas on index change
     }, 10000);
 
     return () => clearInterval(interval);
-  }, [loading, activeIndex, personas]);
+  }, [loading, activeIndex]);
 
   useEffect(() => {
     if (videoRefs.current[activeIndex]) {
-      videoRefs.current[activeIndex].currentTime = videoCurrentTimes[activeIndex];
-      videoRefs.current[activeIndex].play();
+      videoRefs.current[activeIndex].currentTime = videoCurrentTimes[activeIndex]; // Set to stored time
+      videoRefs.current[activeIndex].play(); // Ensure the video plays
     }
   }, [activeIndex]);
 
   const handleVideoTimeUpdate = (index: number) => {
     if (videoRefs.current[index]) {
-      const currentTime = videoRefs.current[index].currentTime;
+      const currentTime = videoRefs.current[index].currentTime; // Get current time
       setVideoCurrentTimes((prev) => {
         const newTimes = [...prev];
-        newTimes[index] = currentTime;
+        newTimes[index] = currentTime; // Store time for the specific video
         return newTimes;
       });
     }
@@ -120,9 +85,9 @@ const Banner = ({
       <div className="flex items-center justify-center relative md:mb-12">
         {/* Left Persona Card */}
         <div className="hidden xl:block absolute left-2 z-20" style={{ bottom: '-1rem' }}>
-          {selectedPersonas[0] && (
+          {_personas[0] && (
             <HeroPersonaCard
-              persona={selectedPersonas[0]}
+              persona={_personas[0]}
               index={0}
             />
           )}
@@ -132,7 +97,7 @@ const Banner = ({
         <div className="h-[380px] bg-contain overflow-hidden relative w-[1200px]">
           <video
             ref={(el) => {
-              videoRefs.current[activeIndex] = el;
+              videoRefs.current[activeIndex] = el; // Attach the correct ref
             }}
             preload="auto"
             className="object-cover object-center select-none w-full h-[380px]"
@@ -141,10 +106,10 @@ const Banner = ({
             playsInline
             muted
             loop
-            onTimeUpdate={() => handleVideoTimeUpdate(activeIndex)}
+            onTimeUpdate={() => handleVideoTimeUpdate(activeIndex)} // Track time updates
             key={`hero-video-${activeIndex}`}
           >
-            <source src={videoLinks[activeIndex]} type="video/mp4" />
+            <source src={activeItem.videoLink} type="video/mp4" />
           </video>
 
           <div
@@ -165,7 +130,7 @@ const Banner = ({
               <Typography 
                 key={`typing-${activeIndex}`} 
                 variant={"h3"} className="typing-effect">
-                {randomTitles[activeIndex % randomTitles.length]} {/* Display random title */}
+                {activeItem.title}
               </Typography>
             </div>
           </div>
@@ -173,9 +138,9 @@ const Banner = ({
 
         {/* Right Persona Card */}
         <div className="hidden xl:block absolute right-2 z-20" style={{ bottom: '-1rem' }}>
-          {selectedPersonas[1] && (
+          {_personas[1] && (
             <HeroPersonaCard
-              persona={selectedPersonas[1]}
+              persona={_personas[1]}
               index={1}
             />
           )}
@@ -184,7 +149,7 @@ const Banner = ({
 
       {/* Mobile Persona Cards */}
       <div className="-mt-3 md:mt-4 flex gap-4 xl:hidden ">
-        {selectedPersonas.map((persona, index) => (
+        {_personas.map((persona, index) => (
           <HeroPersonaCard_Mobile
             key={`hero-persona-${persona.persona_id}`}
             persona={persona}
@@ -197,4 +162,3 @@ const Banner = ({
 };
 
 export default Banner;
- 
