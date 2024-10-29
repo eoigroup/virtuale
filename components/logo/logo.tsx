2e707d5ade55a69825a86bf376a4872886e8b248
eoigroup <168847056+eoigroup@users.noreply.github.com>
2024-10-29 01:06:23 +0000

    Update: Logo scroll

--- components/logo/logo.tsx
+++ components/logo/logo.tsx
@@ -1,14 +1,43 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { Typography } from "../ui/typography";
 import Link from "next/link";
 import { AudioLines } from "lucide-react";
+import { cn } from "@/lib/utils";
 
 
 const Logo = () => {
+  const [isScrolled, setIsScrolled] = useState(false);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      if (window.scrollY > 50) {
+        setIsScrolled(true);
+      } else {
+        setIsScrolled(false);
+      }
+    };
+
+    window.addEventListener("scroll", handleScroll);
+
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, []);
+
+
+
   return (
 <Link href={"/"} className="flex-1">
   <Typography variant={`h3`} className="font-bold" as={"div"}>
-    <div className="flex flex-col pt-6 md:pt-0"> {/* Change to flex-col for vertical alignment */}
+
+  <div
+      className={cn(
+        "flex flex-col pt-6 md:pt-0",
+        { "pt-0": isScrolled }
+      )}
+    > {/* Change to flex-col for vertical alignment */}
+
+
       <div className="flex items-center"> {/* Align items in the center */}
         <AudioLines size={12} className="text-yellow-400 font-bold mr-2 mt-0" />
         Virtuale.ai
